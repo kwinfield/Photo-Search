d800a62fefe74600970141ccf01ac929682e3633
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2019-07-15 10:29:06 -0400

    Updates

---
+++ precache-manifest.7da41c73d13bebb829ff058a6eeec918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5222be39ad1dd028568db6e88db0c1e",
+    "url": "/Photo-Search/index.html"
+  },
+  {
+    "revision": "2852407a0c9e53b74ab7",
+    "url": "/Photo-Search/static/css/main.465765bc.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/Photo-Search/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "2852407a0c9e53b74ab7",
+    "url": "/Photo-Search/static/js/main.c431793f.chunk.js"
+  },
+  {
+    "revision": "48f1444d0d2698771a82",
+    "url": "/Photo-Search/static/js/runtime~main.bc80ef42.js"
+  }
+]);
